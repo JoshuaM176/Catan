05a28482c2094fd949eef993a6561b311b6123a3
JoshuaM176 <joshuam18745@gmail.com>
2026-05-29 09:48:02 -0500

    actionManager docs

--- src/main/java/luis/josh/catan/host/game/actionmanager/ActionManager.java
+++ src/main/java/luis/josh/catan/host/game/actionmanager/ActionManager.java
@@ -13,6 +13,10 @@ import luis.josh.catan.host.HostLogger;
 import luis.josh.catan.host.game.actions.Action;
 import luis.josh.catan.host.game.actions.messages.EventResponses;
 
+/**
+ * Keeps track of available actions and handles executing action from input data.
+ * @version 1.0
+ */
 public class ActionManager {
 
     private Player[] players;
@@ -21,12 +25,21 @@ public class ActionManager {
     private Supplier<Integer> turn;
     private static final Logger logger = HostLogger.getLogger(ActionManager.class);
 
+    /**
+     * @param players The list of players for the current game.
+     * @param actionMap Map the action names to corresponding Action object.
+     */
     public ActionManager(Player[] players, Map<String, Action> actionMap) {
         this.players = players;
         this.actionMap = new HashMap<>(actionMap);
         this.turn = () -> -1;
     }
 
+    /**
+     * @param players The list of players for the current game.
+     * @param actions List of Action objects for the current game. Names will
+     * automatically be set to className as camelCase.
+     */
     public ActionManager(Player[] players, Action[] actions) {
         this.players = players;
         for(Action action: actions) {
@@ -34,12 +47,22 @@ public class ActionManager {
         }
     }
 
+    /**
+     * Sets waitForTurn property which prevents actions from executing if the player
+     * number does not match the current turn.
+     * @param turn Supplier for the current turn.
+     * @return Itself
+     */
     public ActionManager setWaitForTurn(Supplier<Integer> turn) {
         this.turn = turn;
         waitForTurn = false;
         return this;
     }
 
+    /**
+     * Add an action automatically setting name to the action's className as camelCase.
+     * @param action Action to execute when the name is used
+     */
     public void addAction(Action action) {
         String name = action.getClass().getSimpleName();
         char firstLetter = name.charAt(0);
@@ -47,10 +70,21 @@ public class ActionManager {
         addAction(camelCaseName, action);
     }
 
+    /**
+     * Adds an action to the map of available actions.
+     * @param name Name that will be used to reference the action
+     * @param action Action to execute when that name is used
+     */
     public void addAction(String name, Action action) {
         actionMap.put(name, action);
     }
 
+    /**
+     * Executes the action from the given data. Data should be formatted as
+     * {"action": "actionName", "player": playerNum, "data": {actionData}}
+     * @param data The data containing information about the action to execute
+     * @return Array of EventResponses resulting from the executed action
+     */
     public JSONObject[] executeAction(JSONObject data) {
         int playerNum = (int)data.get("player");
         if(waitForTurn && (playerNum != turn.get().intValue())) {
@@ -71,6 +105,11 @@ public class ActionManager {
         return results;
     }
 
+    /**
+     * Replaces "self" in {"players": "self"} with [playerNum].
+     * @param jsonObject The json object to replace self in
+     * @param playerNum The playerNum represented by self
+     */
     @SuppressWarnings("unchecked")
     private void replaceSelf(JSONObject jsonObject, int playerNum) {
         if(((String)jsonObject.get("players")).equals("self")) {
